8eb6002f31ccdc0df1d11e13280c1d14525d05ae
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-13 14:53:54 +0100

    fix: update advantage component

--- src/components/Advantage.tsx
+++ src/components/Advantage.tsx
@@ -17,18 +17,26 @@ const Advantage = () => {
           Our cutting-edge video conferencing app offers:
         </p>
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-between gap-20 mt-12">
-        <main className="grid sm:grid-cols-2 gap-12 md:w-4/6 ">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-20 mt-16 md:mt-12">
+        <main className="grid sm:grid-cols-2 gap-14 md:w-4/6 ">
           {advantages.map((advantage) => (
             <div key={advantage.id} className="flex flex-col gap-4">
-              <Image src={advantage.img} alt={advantage.title} />
-              <h3 className="text-xl font-bold text-gray-900">{advantage.title}</h3>
+              <div className=" bg-gray-50 border  border-gray-200 p-2 rounded-full w-10 h-10">
+                <Image src={advantage.img} alt={advantage.title}  className="z-50"/>
+              </div>
+              <h3 className="text-xl font-bold text-gray-900">
+                {advantage.title}
+              </h3>
               <p className="text-gray-600">{advantage.desc}</p>
             </div>
           ))}
         </main>
         <aside className="hidden md:block w-2/6 relative mt-12">
-          <Image src={arrow} alt="arrow" className="absolute -top-32 w-48 left-[13%]" />
+          <Image
+            src={arrow}
+            alt="arrow"
+            className="absolute -top-32 w-48 left-[13%]"
+          />
           <Image src={people} alt="people" className="" />
         </aside>
       </div>
