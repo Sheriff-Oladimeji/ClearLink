be7a347e3c4dcecf0ee545a378a1505aaf6e61a1
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-13 20:29:27 +0100

    feat: add desktop navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "public/logo.svg";
+import Image from "next/image";
+import Button from "./ui/Button";
 const Navbar = () => {
   return (
-    <nav className="bg-gray-100 border  border-gray-300 mt-12 container py-4 rounded-full">ClearLink.</nav>
+    <nav className="bg-gray-100 border  border-gray-300 mt-12 container  rounded-full ">
+      <div className="md:flex justify-between items-center w-[95%] mx-auto hidden py-2">
+        <div className="flex items-center gap-1">
+          <Image src={Logo} alt=""  />
+          <p className="text-gray-900 font-semibold text-lg">ClearLink</p>
+        </div>
+        <ul className="text-gray-500 flex gap-8 items-center">
+          <li>Products</li>
+          <li>Solutions</li>
+          <li>Resources</li>
+          <li>Pricing</li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <Button
+            value="Talk to sales"
+            styles="text-gray-900 bg-white border border-gray-400"
+            link=""
+          />
+          <Button
+            value="Sign up for free"
+            styles="text-white bg-blue "
+            link=""
+          />
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
